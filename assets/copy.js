82a96208fa2f1d86f062e053bc34eef72def76ed
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2021-02-22 03:59:22 -0800

    Updated

---
+++ assets/copy.js
@@ -0,0 +1,8 @@
+
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert("Copied the text: " + copyText.value);
+}
