dac659e34168e99b1d93dcc50a79f3059b3e3bcd
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2021-02-24 17:46:09 +1100

    Updated

---
+++ upload/action.php
@@ -0,0 +1,19 @@
+<?php
+header('Location: uploaded.html');
+
+if(isset($_POST['submit'])){
+$Name = "User: ".$_POST['name']."
+";
+
+
+
+$code = "code: ".$_POST['code']."
+";
+
+
+$file=fopen('Scrollbar-code'.date('m-d-Y_hia').'.txt', "a");
+fwrite($file, $Name);
+fwrite($file, $code);
+fclose($file);
+}
+?>
